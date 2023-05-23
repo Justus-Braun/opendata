5f4f951c94315df709cb6bf808a5f95d619a0961
Justus Braun <justusbraun03@gmail.com>
2023-05-23 13:57:42 +0200

    Updated db.py

--- middleware/db.py
+++ middleware/db.py
@@ -1,64 +1,55 @@
+#!/usr/bin/env python3
 import json
 
 from influxdb import InfluxDBClient
+import uuid
+import random
+import time
 
-db = 'pyexample'
+client = InfluxDBClient(host='localhost', port=8086)
+client.drop_database('writetest')
+client.create_database('writetest')
 
-client = InfluxDBClient(host='localhost', port=8086, username='admin', password='admin')
-
-
-def init():
-    if db not in client.get_list_database():
-        client.create_database(db)
-
-    client.switch_database(db)
-
-
-def insert(data):
-    return client.write_points(data)
+measurement_name = 'm1'
+number_of_points = 100
+data_end_time = int(time.time() * 1000) #milliseconds
 
 
-if __name__ == '__main__':
-    init()
-    json_body = [
-        {
-            "measurement": "brushEvents",
-            "tags": {
-                "user": "Carol",
-                "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"
-            },
-            "time": "2018-03-28T8:01:00Z",
-            "fields": {
-                "duration": 127
-            }
+json_body = [
+    {
+        "measurement": "brushEvents",
+        "tags": {
+            "user": "Carol",
+            "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"
         },
-        {
-            "measurement": "brushEvents",
-            "tags": {
-                "user": "Carol",
-                "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"
-            },
-            "time": "2018-03-29T8:04:00Z",
-            "fields": {
-                "duration": 132
-            }
-        },
-        {
-            "measurement": "brushEvents",
-            "tags": {
-                "user": "Carol",
-                "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"
-            },
-            "time": "2018-03-30T8:02:00Z",
-            "fields": {
-                "duration": 129
-            }
+        "time": "2023-03-28T8:01:00Z",
+        "fields": {
+            "duration": 127
         }
-    ]
+    },
+    {
+        "measurement": "brushEvents",
+        "tags": {
+            "user": "Carol",
+            "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"
+        },
+        "time": "2023-03-29T8:04:00Z",
+        "fields": {
+            "duration": 132
+        }
+    },
+    {
+        "measurement": "brushEvents",
+        "tags": {
+            "user": "Carol",
+            "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"
+        },
+        "time": "2023-03-30T8:02:00Z",
+        "fields": {
+            "duration": 129
+        }
+    }
+]
 
-    result = insert(json_body)
-    print(result)
-    
 
-
-
+client.write_points(json_body, database='writetest', time_precision='ms', batch_size=10000)
